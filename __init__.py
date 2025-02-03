552cb3e9f77700245cf424a58aa4678b02f5e708
Ryuukeisyou <jingxiang.liu@live.com>
2025-02-03 23:13:03 +0800

    add folder to folder_paths

--- __init__.py
+++ __init__.py
@@ -12,6 +12,7 @@ if not os.path.exists(face_parsing_path):
     os.makedirs(face_parsing_path)
 if not os.path.exists(ultralytics_bbox_path):
     os.makedirs(ultralytics_bbox_path)
+folder_paths.add_model_folder_path("ultralytics_bbox", ultralytics_bbox_path)
 if not os.path.exists(os.path.join(face_parsing_path, "model.safetensors")):
     download_url("https://huggingface.co/jonathandinu/face-parsing/resolve/main/model.safetensors?download=true", face_parsing_path, "model.safetensors")
 if not os.path.exists(os.path.join(face_parsing_path, "config.json")):
